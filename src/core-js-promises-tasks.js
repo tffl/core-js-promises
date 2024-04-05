2a8b07cc602b3ed175ac10059e5d04fbe44c0aba
ntsh <106670334+tffl@users.noreply.github.com>
2024-04-05 16:06:38 +0300

    feat: queuPromises solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -139,8 +139,12 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce((queue, promise) => {
+    return queue.then((result) => {
+      return promise.then((value) => result + value);
+    });
+  }, Promise.resolve(''));
 }
 
 module.exports = {
