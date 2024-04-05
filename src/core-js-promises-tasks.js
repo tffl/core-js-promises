09a087d53f21dfdd3201f23402bdddcb305ba1fe
ntsh <106670334+tffl@users.noreply.github.com>
2024-04-05 12:19:23 +0300

    feat: getAllResult solution

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -112,8 +112,13 @@ function getAllOrNothing(promises) {
  * [Promise.resolve(1), Promise.resolve(2), Promise.resolve(3)] => Promise fulfilled with [1, 2, 3]
  * [Promise.resolve(1), Promise.reject(2), Promise.resolve(3)]  => Promise fulfilled with [1, null, 3]
  */
-function getAllResult(/* promises */) {
-  throw new Error('Not implemented');
+function getAllResult(promises) {
+  const allPromises = promises.map((promise) =>
+    Promise.resolve(promise)
+      .then((value) => value)
+      .catch(() => null)
+  );
+  return Promise.all(allPromises);
 }
 
 /**
